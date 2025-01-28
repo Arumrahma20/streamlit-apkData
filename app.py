084f9bc7a60f1cdb5217927dadf236a17e95429d
Arum Rahmadhani <163228132+Arumrahma20@users.noreply.github.com>
2025-01-28 20:54:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,15 +16,7 @@ st.set_page_config(
     initial_sidebar_state="expanded")
 
 # Fungsi untuk menghubungkan ke database PostgreQL
-def connect_db():
-    conn = psycopg2.connect(
-        dbname='callcenter', 
-        user='postgres', 
-        password='123456', 
-        host='localhost', 
-        port='5432'
-    )
-    return conn
+conn = st.connection("postgresql", type="sql")
 
 # Fungsi untuk mengambil data dari database berdasarkan query
 def fetch_data_from_db(query):
